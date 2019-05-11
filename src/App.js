6cfc0dea03e149686e83206054eaca71aee6c96e
Nicolas A. Mendez <nmendez@bsaenz.com.ar>
2019-05-10 21:55:37 -0300

    cambios en login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route , Switch } from 'react-router-dom';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
 //Componentes
 import PasswordChange from './components/PasswordChange';
@@ -7,19 +8,23 @@ import Login from './components/Login';
 import Home from './components/Home';
 import Error404 from './components/Error404';
 
+const theme = createMuiTheme();
+
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
 
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Login} exact/>
-          <Route path="/passchange" component={PasswordChange} />
-          <Route path="/home" component={Home} />
-          <Route component={Error404} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Login} exact/>
+            <Route path="/passchange" component={PasswordChange} />
+            <Route path="/home" component={Home} />
+            <Route component={Error404} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import { CssBaseline, Typography, Paper, FormControl, InputLabel, Input, Button } from '@material-ui/core';
+import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
 
-const classes = styles => {
 
-}
+const classes = styles => ({
+    paper: {
+        boxShadow: 'unset',
+        backgroundColor: '#fafafa'
+    },
+    form: {
+        margin: 'auto',
+        maxWidth: 500,
+        textAlign: 'center'
+    },
+    buttonInicio: {
+        marginTop: 10,
+        marginRight: 25
+    },
+    buttonRegistro: {
+        marginTop: 10,
+        marginLeft: 25
+    }
+})
 
 class Login extends Component {
     render () {
+        const classes = this.props.classes;
         return (
             <main className={classes.main}>
                 <CssBaseline />
                 <Paper className={classes.paper}>
-                    <Typography component="h1" variant="h5">
+                    <Typography component="h1" variant="h2">Buscador de películas</Typography>
+                    <Typography component="h2" variant="h5">
                         Iniciar sesión
                     </Typography>
                     <form className={classes.form}>
@@ -23,7 +43,10 @@ class Login extends Component {
                             <InputLabel htmlFor="password">Clave</InputLabel>
                             <Input id="password" type="password" name="password" autoComplete="current-password"/>
                         </FormControl>
-                        <Button type="submit" fullWidth color="primary" variant="contained" disableRipple>Iniciar sesion</Button>
+                        <div className={classes.buttons}>
+                            <Button className={classes.buttonInicio} type="submit" color="primary" variant="contained" disableRipple>Iniciar sesion</Button>
+                            <Button className={classes.buttonRegistro} type="submit" color="primary" variant="contained" disableRipple>Registrarse</Button>
+                        </div>
                     </form>
                 </Paper>
             </main>
@@ -31,4 +54,4 @@ class Login extends Component {
     }
 }
 
-export default Login;
+export default withStyles(classes)(Login);
