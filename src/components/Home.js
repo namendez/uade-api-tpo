2a34a528516aef53b82885150bafa12e6f92d50a
Nicolas A. Mendez <nmendez@bsaenz.com.ar>
2019-05-15 20:56:25 -0300

    agrego infinite scroll al home

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,8 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import CommentIcon from '@material-ui/icons/Comment';
 import SearchIcon from '@material-ui/icons/Search';
 import { FormControl, Card, CardMedia, CardContent, Typography , Grid, IconButton, Tooltip } from '@material-ui/core';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 const OMDB_URL="http://www.omdbapi.com/?type=movie"
 const OMDB_APIKEY=""
@@ -110,6 +112,9 @@ class Home extends Component {
             movies: false,
             page: 1,
             snackBarOpen: false,
+            hasMore: false,
+            searchTerm: '',
+            cantidadMostrada: 0
         }
     }
     handleSnackbarClose = (event, reason) => {
@@ -121,21 +126,46 @@ class Home extends Component {
       };
 
     handleSearchSubmit = (event) => {
-        const palabraBuscada = event.target.children[0].children[0].children[0].value;
-        const pagina = this.state.page;
-        const url = OMDB_URL + '&apikey=' + OMDB_APIKEY + '&s=' + palabraBuscada + '&page=' + pagina;
+        const searchTerm = event.target.children[0].children[0].children[0].value;
+        const page = this.state.page;
+        const url = OMDB_URL + '&apikey=' + OMDB_APIKEY + '&s=' + searchTerm + '&page=' + page;
         
         fetch(url).then(response => response.json()).then(result => {
             const movies = result;
-            if (movies.Response === "True")
-                this.setState({movies: movies});
-            else
+            if (movies.Response === "True"){
+                const hasMore = movies.totalResults > 10 ? true : false;
+                this.setState({movies: movies, 
+                                page: 1, 
+                                hasMore, 
+                                searchTerm,
+                                cantidadMostrada: movies.Search.length});
+            } 
+            else 
                 this.setState({snackBarOpen: true});
         });
         
         event.preventDefault();
     };
 
+    fetchMoreData = () => {
+        const searchTerm = this.state.searchTerm;
+        const page = this.state.page + 1;
+        const url = OMDB_URL + '&apikey=' + OMDB_APIKEY + '&s=' + searchTerm + '&page=' + page;
+
+        fetch(url).then(response => response.json()).then(result => {
+            //Creo un nuevo objeto movies a partir del original, pisandole el atributo Search
+            const movies = Object.assign(this.state.movies, {Search: this.state.movies.Search.concat(result.Search)});
+            const cantidadMostrada = this.state.cantidadMostrada + result.Search.length;
+            const hasMore = cantidadMostrada === Number(result.totalResults) ? false : true;
+
+            this.setState({
+                movies: movies,
+                page,
+                hasMore,
+                cantidadMostrada
+            });
+        });
+    }
 
     render () {
         const classes = this.props.classes;
@@ -158,12 +188,22 @@ class Home extends Component {
                         </IconButton>
                     </Toolbar>
                 </AppBar>
-                    <Grid container className={classes.moviePanel} >
-                        {this.state.movies && this.state.movies.Search.map(movie =>
-                            <Grid item xs={12} sm={6} md={4} lg={3} key={movie.imdbID}>
-                                <MovieBox classes={classes} movie={movie} />
-                            </Grid>)}
-                    </Grid>
+                    
+                    {this.state.movies && 
+                        <InfiniteScroll className={classes.moviePanel}
+                            dataLength={this.state.cantidadMostrada}
+                            next={this.fetchMoreData}
+                            hasMore={this.state.hasMore}
+                            loader="Cargando..."
+                            endMessage={<Typography component="h1" variant="display3">FIN DE LOS RESULTADOS</Typography>}>
+                            <Grid container >
+                                {this.state.movies.Search.map(movie =>
+                                    <Grid item xs={12} sm={6} md={4} lg={3} key={movie.imdbID}>
+                                        <MovieBox classes={classes} movie={movie} />
+                                    </Grid>)}
+                            </Grid>
+                        </InfiniteScroll>
+                    }
             </React.Fragment>
         );
     }
